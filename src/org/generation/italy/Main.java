c3e0dd4f1fef1f99d3a152952287bba4662f5700
Samuele Dascalu <samuele.dscalu.0204@gmail.com>
2023-11-13 12:46:49 +0100

    aggiunto un commento a riga 9

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		//Programma per la fatturazione di un'officina
 		Scanner scanner = new Scanner(System.in);
 		
 		String stringaInserita, risposta;
